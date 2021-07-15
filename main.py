b52f01a314a8b95d356738e20ce291c1efc040c4
idan054 <idanbit80@gmai.com>
2021-07-15 23:19:17 +0300

    V1.1.1 Spider Admin Main.Beta.BugsFixes
    Main:
    =====
    .1 push to github
    
    Beta:
    =====
    .1 main.py PUSH BASED FLASK
    .1 flask_tests.py GET & USE VALUES FROM FORM
    
    BugFixes:
    =========
    .1 renew github Token

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+from flask import Flask
 from github import Github, InputGitAuthor
 from datetime import datetime
 
 # current date and time
 now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-file_path = "main.py"
-token = "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"
+file_path = "index.json"
+token = "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"
 g = Github(token)
-repo = g.get_repo("idan054/BranchesTesting")
+repo = g.get_repo("idan054/Spider3DFlux")
 
 # file = repo.get_contents(file_path, ref="master")  # Get file from branch
 # data = file.decoded_content.decode("utf-8")  # Get raw string data
@@ -26,9 +27,17 @@ def push(path, message, content, branch):
     try:  # If file already exists, update it
         contents = repo.get_contents(path, ref=branch)  # Retrieve old file to get its SHA and path
         repo.update_file(contents.path, message, content, contents.sha, branch=branch, author=author)  # Add, commit and push branch
-        print("File has been updated!")
+        return "File has been updated!"
     except:  # If file doesn't exist, create it
         repo.create_file(path, message, content, branch=branch, author=author)  # Add, commit and push branch
-        print("File has been added!")
+        return "File has been added!"
 
-push(file_path, f"Latest update on {now} (by .py)", data, "master")
+push(file_path, f"Latest update on {now} (by .py)", data, "main")
+
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    resp = push(file_path, f"Latest update on {now} (by .py)", data, "main")
+    return resp
+if __name__ == '__main__':
+    app.run()
