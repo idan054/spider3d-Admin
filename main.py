82771f6e84900111de76eb5758bd6ae3c42d5bd8
idan054 <idanbit80@gmai.com>
2021-07-17 18:28:17 +0300

    A

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from datetime import datetime
 # current date and time
 now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 file_path = "index.json"
-token = "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"
-g = Github(token)
+token_prt1 = "ghp_M4wHc747B813CJl7E"
+token_prt2 = "mtRoEFJdAUsy42sb4Y5"
+g = Github(f"{token_prt1}{token_prt2}")
 repo = g.get_repo("idan054/Spider3DFlux")
 
 # file = repo.get_contents(file_path, ref="master")  # Get file from branch
